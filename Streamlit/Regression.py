cec250c0e8edabae6134736413aa7f81d28fd063
XXOlsen <cph-xo@cphbusiness.dk>
2024-03-13 20:29:08 +0100

    omskrivning + figur 1

--- Streamlit/Regression.py
+++ Streamlit/Regression.py
@@ -2,14 +2,26 @@ import streamlit as st
 import pandas as pd
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import numpy as np
 from DataLoader import load_data
 
 def show_Regression():
-    st.title('Data regression')
+    st.title('Linear Regression')
     st.write("On this page, we will take a look at the dataset and try to visualize it. We will use the data to create some graphs and see if we can find any patterns or trends.")
 
     df = load_data()  # Load your DataFrame
 
+    aqi_value(df)
+
+    multiple_linear_regression(df)
+
+    
+
+# AQI value
+def aqi_value(df):     
     st.title('Analysis of air quality in C40 cities using K-means clustering')
 
     feature_cols = ['City Latitude', 'City Longitude']
@@ -37,9 +49,39 @@ def show_Regression():
 
     st.pyplot(fig)
 
-# figure 1 X
-    
 
+# figure 1
+def multiple_linear_regression(df):
+    st.title('Multiple Linear Regression')
+    feature_cols = ['City Latitude', 'City Longitude']
+    X = df[feature_cols]
+    y = df['AQI Value']
+    
+    # Splitting the dataset
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1, test_size=0.15)
+
+    linreg = LinearRegression()
+    linreg.fit(X_train, y_train)
+
+    y_predicted = linreg.predict(X_test)
+
+    # Plotting the regression results
+    fig2, ax2 = plt.subplots()
+    ax2.scatter(y_test, y_predicted, color='blue')
+    ax2.set_xlabel('City')
+    ax2.set_ylabel('AQI Value')
+    ax2.set_title('Multiple Linear Regression Results')
+
+    st.pyplot(fig2)
+
+    # Displaying metrics
+    mse = mean_squared_error(y_test, y_predicted)
+    rmse = np.sqrt(mse)
+    r2 = r2_score(y_test, y_predicted)
+
+    st.write(f"Mean Squared Error (MSE): {mse}")
+    st.write(f"Root Mean Squared Error (RMSE): {rmse}")
+    st.write(f"R-squared (R2 ): {r2}")
 
 
 # figure 2 X
